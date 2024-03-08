c3fb64430dd6db01cf26533aa03f9e7ea38985bd
“Pavel_V” <torvokard@gmail.com>
2024-03-08 11:45:51 +0300

    третья задача

---
+++ Desktop/sert/Z3/Program.cs
@@ -0,0 +1,14 @@
+﻿  void print(int [] A, int n) 
+    { 
+      if (n > 0) 
+      { 
+      	Console.Write(A[n] + " "); 
+      	print(A, n - 1); 
+      } 
+      else Console.WriteLine(A[0]); 
+    } 
+     
+     
+      int [] num = {1, 2, 5, 0, 10, 34}; 
+      print(num, num.Length - 1); 
+    
